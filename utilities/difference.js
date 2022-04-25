5cae86c6c7f955f7d2577e25a87dade3051776d8
efraintinoco <oakcliff_21@yahoo.com>
2022-04-24 20:50:41 -0500

    change function

--- utilities/difference.js
+++ utilities/difference.js
@@ -3,7 +3,7 @@ import { difference } from "lodash"
 
 export default function differenceOfArrays(arrayOne, arrayTwo) {
     const differenceOfArrays = difference(arrayOne, arrayTWo)
-    return `The difference between ${arrayOne} and ${arrayTwo} is ${difference(differenceOfArrays)}`
+    return `The difference between ${arrayOne} and ${arrayTwo} is ${differenceOfArrays}`
 }
 
 
