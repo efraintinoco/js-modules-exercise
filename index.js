d5f62ca0ba129d36de7cf5018b39411833c0625b
efraintinoco <oakcliff_21@yahoo.com>
2022-04-24 20:36:16 -0500

    add parcel

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+import differenceOfArrays from "./utilities/difference"
+
 const arrayOne = [1, 2, 3]
 const arrayTwo = [2, 3, 4]
+
+const p = document.querySelector("#target")
+p.textContent = differenceOfArrays(arrayOne, arrayTwo)
